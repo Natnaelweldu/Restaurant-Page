d4218308a4118e3638ed47ca26b0902e6c931373
Nate_Theodore <natnael.welduu@gmail.com.github.com>
2025-01-02 22:06:55 +0300

    Add and setup htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-}
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ]
+};
